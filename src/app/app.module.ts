a2ac7b0b89f4b847cf72f826df459104de9f7c49
monkdev1988 <mayank.dev.parihar1988@gmail.com>
2020-01-04 05:45:15 +0100

    inital service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/Forms';
+
+import { UserdataService } from '../app/services/userdata.service';
+
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UsersComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    UserdataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { User } from '../../models/User';
+import { UserdataService } from '../../services/userdata.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  user: User = {
+    firstName: '',
+    lastName: '',
+    email: ''
+  };
+  users: User[];
+  showExtended = true;
+  loaded = false;
+  enableAdd = false;
+  showUserForm = false;
+  @ViewChild('userForm', {static: true}) form: any;
+
+  constructor(private userDataService: UserdataService) { }
+
+  ngOnInit() {
+
+      this.users = this.userDataService.getUsers();
+
+      this.loaded = true;
+  }
+
+  // addUser() {
+  //   this.user.isActive = true;
+  //   this.user.registered = new Date();
+
+  //   this.users.unshift(this.user);
+
+  //   this.user = {
+  //     firstName: '',
+  //     lastName: '',
+  //     email: ''
+  //   }
+  // }
+
+  addUser(user: User) {
+    this.users.unshift(user);
+  }
+
+  onSubmit({value, valid}: {value: User, valid: boolean}) {
+    if(!valid){
+      console.log('Form is not valid');
+    } else {
+      value.isActive = true;
+      value.registered = new Date();
+      value.hide = true;
+
+      this.addUser(value);
+
+      this.form.reset();
+    }
+  }
+
+
+}
